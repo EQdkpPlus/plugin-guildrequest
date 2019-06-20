6ee150d6f7727d772f65d4319bad7460fceaa450
GodMod <godmod23@web.de>
2019-06-20 12:01:00 +0200

    Group notifications

--- guildrequest_plugin_class.php
+++ guildrequest_plugin_class.php
@@ -28,7 +28,7 @@ if (!defined('EQDKP_INC')){
   +--------------------------------------------------------------------------*/
 class guildrequest extends plugin_generic {
 
-	public $version				= '1.1.8';
+	public $version				= '1.1.9';
 	public $build				= '';
 	public $copyright			= 'GodMod';
 
